f716902da5e3abc43e02ae54ba83cb8054260a76
Rohit Thorat <109894186+roht2103@users.noreply.github.com>
2023-07-20 22:26:36 +0530

    show output

--- main.js
+++ main.js
@@ -8,6 +8,12 @@ let img3=document.querySelector("#img3")
 let img4=document.querySelector("#img4")
 let img5=document.querySelector("#img5")
 let img6=document.querySelector("#img6")
+let sol=document.querySelector(".sol")
+let solH1=document.querySelector(".sol-h1")
+let li1=document.querySelector(".li1")
+let li2=document.querySelector(".li2")
+let li3=document.querySelector(".li3")
+let li4=document.querySelector(".li4")
 
 form.addEventListener("submit",formHandeller);
 
@@ -44,6 +50,7 @@ function calculateProfitAndLoss(initial,quantity,current){
             img4.classList.add("off")
             img5.classList.add("off")
             img6.classList.add("off")
+            sol.style.visibility="visible"
         }
         else if (lossPercentage>50) {
             img1.classList.add("off")
@@ -52,6 +59,7 @@ function calculateProfitAndLoss(initial,quantity,current){
             img4.classList.add("off")
             img5.classList.add("off")
             img6.classList.remove("off")
+            sol.style.visibility="visible"
         }
         
     }
@@ -62,22 +70,30 @@ function calculateProfitAndLoss(initial,quantity,current){
         console.log("profitPercentage="+profitPercentage+"%");
         console.log("profit="+profit)
         console.log("amount="+amount);
-
+        sol.style.visibility="visible"
+        li1.innerHTML="Total Amount Invested: "+totalAmountInvested
+        li2.innerHTML="Profit gained: "+profit
+        li3.innerHTML="Profit Percentage: "+profitPercentage+"%"
+        li4.innerHTML="Amount: "+amount
         if (profitPercentage<50) {
+            solH1.innerHTML="Great work! Keep Investing"
             img1.classList.add("off")
             img2.classList.add("off")
             img3.classList.add("off")
             img4.classList.add("off")
             img5.classList.remove("off")
             img6.classList.add("off")
+            
         }
         if (profitPercentage>50) {
+            solH1.innerHTML="Great work! Going to Moon"
             img1.classList.add("off")
             img2.classList.add("off")
             img3.classList.add("off")
             img4.classList.remove("off")
             img5.classList.add("off")
             img6.classList.add("off")
+            sol.style.visibility="visible"
         }
     }
     else{
@@ -87,6 +103,7 @@ function calculateProfitAndLoss(initial,quantity,current){
         img4.classList.add("off")
         img5.classList.add("off")
         img6.classList.add("off")
-        console.log("No pain no gain,No gain no pain")
+        sol.style.visibility="visible"
+        solH1.innerHTML= "No pain no gain,No gain no pain"
     }
 }
